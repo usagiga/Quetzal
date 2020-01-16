29bd898109dc7bedb823edc3c2bbf13b02396af7
Usagigawara <54873357+usagiga@users.noreply.github.com>
2020-01-16 20:14:09 +0900

    Wrote empty UserScript.

---
+++ quetzal.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Quetzal
+// @namespace    https://github.com/usagiga/Quetzal
+// @version      1.0
+// @description  Custom CSS for made esa.io pretty.
+// @author       Usagigawara
+// @match        *.esa.io/posts/*
+// @grant        none
+// @run-at        document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Define CSS
+    let css = `
+
+`;
+
+    // Apply CSS into <head>
+    let styleElem = document.createElement("style");
+    styleElem.type = "text/css";
+    styleElem.innerText = css;
+    document.head.appendChild(styleElem);
+})();
