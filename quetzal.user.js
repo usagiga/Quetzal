f9baa6510e5f99d829400d904bc9bc895714db9d
Usagigawara <54873357+usagiga@users.noreply.github.com>
2020-01-23 21:27:25 +0900

    Made centering posts smooth

--- quetzal.user.js
+++ quetzal.user.js
@@ -1,27 +1,75 @@
 // ==UserScript==
 // @name         Quetzal
 // @namespace    https://github.com/usagiga/Quetzal
-// @version      1.0
+// @version      1.1
 // @description  Custom CSS for made esa.io pretty.
 // @author       Usagigawara
 // @match        *.esa.io/posts/*
 // @grant        none
-// @run-at        document-start
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Define CSS
-    let css = `
+    let applyCSS = () => {
+       // Define CSS
+       let css = `
 .layout-post__main {
+    margin: 0 auto;
+}
+
+.layout-post__aside-content {
     margin: 0 auto;
 }
 `;
 
-    // Apply CSS into <head>
-    let styleElem = document.createElement("style");
-    styleElem.type = "text/css";
-    styleElem.innerText = css;
-    document.head.appendChild(styleElem);
+        // Apply CSS into <head>
+        let headElem = document.querySelector("head");
+        let styleElem = document.createElement("style");
+        styleElem.type = "text/css";
+        styleElem.innerText = css;
+        headElem.insertAdjacentElement("afterend", styleElem);
+    };
+
+    // Event handlers
+    let provisionToCLoaded = (container, elem) => {
+        let postContElem = document.querySelector(".layout-post__main");
+        let tocContElem = container;
+        let tocElem = elem;
+
+        // Move ToC to top of the article
+        postContElem.insertAdjacentElement("afterbegin", tocElem);
+        tocContElem.remove();
+    };
+    let provisionDocStart = () => {
+        applyCSS();
+    };
+    let provisionDocIdle = ev => {
+        // Subscribe events (after DOMContentLoaded)
+
+        // Provision when loaded ToC
+        let tocObsCallback = (mrs, ob) => {
+            mrs.forEach(mr => {
+                if (mr.type !== "childList") return;
+                mr.addedNodes.forEach(v => {
+                    if (typeof(v.classList) === "undefined") return;
+                    if (v.classList.contains("toc-box")) {
+                        // Disconnect Mutation
+                        ob.disconnect();
+
+                        provisionToCLoaded(mr.target, v);
+                        return;
+                    }
+                });
+            });
+        }
+        let tocContElem = document.querySelector(".layout-post__toc");
+        let tocObserver = new MutationObserver(tocObsCallback);
+        tocObserver.observe(tocContElem, { childList: true });
+    };
+
+    // Subscribe events
+    provisionDocStart(); // Provision when document-start
+    window.addEventListener("DOMContentLoaded", provisionDocIdle); // Provision when document-idle
 })();
